f434561877c5f1d90e33257985a8c6c8c0eaacce
Harish Kumar N <66734043+harishprp@users.noreply.github.com>
2021-05-31 18:51:21 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,17 @@
+pip install lolcat
+echo '
+       __Hey guys :)
+   _  |@@|             
+  / \ \--/ __
+  ) O|----|  |   __
+ / / \ }{ /\ )_ / _\
+ )/  /\__/\ \__O (__ This is harish
+|/  (--/\--)    \__/
+/   _)(  )(_
+   ----------'| lolcat
+echo " "
+echo "\. ..Script By {H.R.CREATIONS official}.../"| lolcat
+echo " "
 apt-get update -y
 apt-get upgrade -y
 apt install python -y
