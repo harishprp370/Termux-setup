867c29f59cead42c827e4860ab1d2b630fc5b8bb
Harish Kumar N <66734043+harishprp@users.noreply.github.com>
2021-05-31 12:16:07 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,19 @@
+apt-get update -y
+apt-get upgrade -y
+apt install python -y
+apt install python2 -y
+apt install nano -y
+apt install cmatrix -y
+apt install cat -y
+apt install curl -y
+apt install perl -y
+apt install htrrack -y
+apt install openssl -y
+apt install android -y
+apt install toilet -y
+apt install cowsay -y
+apt install git -y
+apt install wget -y
+apt install termux-tools -y
+apt install pip -y
+apt install ruby -y
