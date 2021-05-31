ea904a8f0779daeebe2c1ff6b30b08701a770b99
Harish Kumar N <66734043+harishprp@users.noreply.github.com>
2021-05-31 12:08:18 +0530

    Create banner.sh

---
+++ banner.sh
@@ -0,0 +1,16 @@
+pip install lolcat
+echo '
+       __Hey guys :)
+   _  |@@|             
+  / \ \--/ __
+  ) O|----|  |   __
+ / / \ }{ /\ )_ / _\
+ )/  /\__/\ \__O (__ This is harish
+|/  (--/\--)    \__/
+/   _)(  )(_
+   ----------'| lolcat
+echo " "
+echo "\. ..Script By {H.R.CREATIONS official}.../"| lolcat
+echo " "
+
+
