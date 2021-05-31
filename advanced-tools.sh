69c38e774397e8a275c665d91cf261988c1dffc4
Harish Kumar N <66734043+harishprp@users.noreply.github.com>
2021-05-31 12:25:36 +0530

    Create advanced-tools.sh

---
+++ advanced-tools.sh
@@ -0,0 +1,28 @@
+pip install lolcat
+echo '
+ ****this is very advanced and educational purpous only****'|lolcat
+
+echo " "
+echo "\. ..Script By {H.R.CREATIONS official}.../"| lolcat
+echo " "
+
+apt-get update
+apt-get upgrade
+apt install unstable-repo -y
+echo "\...attempting metasploit framework .../"|lolcat
+echo" "
+apt install metasploit -y
+
+echo "\...attempting metasploit framework .../"|lolcat
+echo" "
+
+apt install git 
+
+git clone https://github.com/rajkumardusad/Tool-X.git
+
+echo "\...attempting metasploit framework .../"|lolcat
+echo" "
+
+https://github.com/sqlmapproject/sqlmap
+
+clear 
