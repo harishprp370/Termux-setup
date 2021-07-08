645c07a34dc47af013ed14a044240295000ee698
Harish Kumar N <66734043+h4ckerharish@users.noreply.github.com>
2021-07-08 06:47:22 -0400

    Update advanced-tools.sh

--- advanced-tools.sh
+++ advanced-tools.sh
@@ -1,28 +1,139 @@
-pip install lolcat
-echo '
- ****this is very advanced and educational purpous only****'|lolcat
+#!/bin/bash
+# version 2.0
 
-echo " "
-echo "\. ..Script By {H.R.CREATIONS official}.../"| lolcat
-echo " "
+clear
 
-apt-get update
-apt-get upgrade
-apt install unstable-repo -y
-echo "\...attempting metasploit framework .../"|lolcat
-echo" "
-apt install metasploit -y
 
-echo "\...attempting Powerful tool is Tool-x.../"|lolcat
-echo" "
+echo ""
+echo -e "FOLLOW ME ON INSTAGRAM"
+echo ""
 
-apt install git 
+echo -e "\e[101m Press Enter \e[0m"
+read a1
 
-git clone https://github.com/rajkumardusad/Tool-X.git
+echo "Loading..." | lolcat
+sleep 2
+clear
 
-echo "\...attempting sqlmap project .../"|lolcat
-echo" "
+toilet -f future "H.R.CREATIONS_OFFICIAL" | lolcat
+echo "-------------------------------------------------------" | lolcat
+echo   "Author   : HR CREATIONS $white" |lolcat
+echo   "Contact  : https://instagram.com/h.r.creations_official $white" |lolcat
+echo   "YouTube  : HR CREATIONS ROYAL $white" |lolcat
+echo   "Blog     : https://hrcreationsofficial.blogspot.com $white" |lolcat
+echo "-------------------------------------------------------" | lolcat
+echo "                           v2.0              " | lolcat
+###################################################
+# CTRL + C
+###################################################
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo  "Detected, Trying To Exit 🚪 ... "
+echo ""
+echo  "⚠️NOT FOR ILLEGEL USE⚠️"
+sleep 1
+echo ""
+echo "BYE BYE 👋👋..." | lolcat
+echo ""
+echo "COME BACK 🔜" | lolcat
+sleep 1
+exit
+}
 
-https://github.com/sqlmapproject/sqlmap
+lagi=01
+while [ $lagi -lt 15 ];
+do
+echo ""
+echo -e "\e[1;101m\e[1;97m01\e[1;101m\e[0m\e[1;96m TOOL-X\e[0m\n";
+echo -e "\e[1;101m\e[1;97m06\e[1;101m\e[0m\e[1;96m INSTAGRAMPHISH\e[0m\n";
+echo -e "\e[1;101m\e[1;97m08\e[1;101m\e[0m\e[1;96m TERMUX-BASIC-PACKAGE\e[0m\n";
+echo -e "\e[1;101m\e[1;97m09\e[1;101m\e[0m\e[1;96m OSIF\e[0m\n";
+echo -e "\e[1;101m\e[1;97m10\e[1;101m\e[0m\e[1;96m INSTAGRAM-BRUTEFORCE\e[0m\n";
+echo -e "\e[1;101m\e[1;97m00\e[1;101m\e[0m\e[1;96m EXIT\e[0m\n";
+echo ""
+echo  "╭─HR CREATIONS" |lolcat
+read -p "╰──►" pil;
 
-clear 
+
+#TOOL-X
+
+
+case $pil in
+01) clear
+toilet -f standard " TECH " -F gay
+git clone https://github.com/Rajkumrdusad/Tool-X
+cd Tool-X
+sh install.aex
+ 
+ 
+;;
+
+
+#INSTAGRAMPHISHING
+
+
+06) clear
+toilet -f standard " TECH " -F gay
+git clone https://github.com/Swetabhsuman8/instagram-hacking-tool
+cd instagram-hacking-tool
+bash instagram-hacking-tool.sh
+
+
+;;
+
+
+#TERMUX-BASIC-PACKAGE
+
+
+08) clear
+toilet -f standard " TECH " -F gay
+https://github.com/rixon-cochi/basic-pkg
+cd basic-pkg
+bash basic.sh
+
+
+;;
+
+
+#OSIF
+
+
+09) clear
+toilet -f standard " TECH " -F gay
+git clone https://github.com/ciku370/OSIF
+cd OSIF
+pip2 install -r requirements.txt
+python2 osif.py
+
+
+;;
+
+
+#INSTAGRAM-BRUTEFORCE
+
+
+10) clear
+toilet -f standard " TECH " -F gay
+git clone https://github.com/linuxkukeren/instagram
+pip install requests
+pip install -r requirement.txt
+pip2 install -r requirement.txt
+python2 linuxkukerenhacking.py
+
+
+;;
+
+
+
+
+00) echo "created by : HR CREATIONS" | lolcat
+exit
+;;
+
+*) echo "sorry, the  option you looking is not found" | lolcat
+esac
+done
+done
+
+=================="
